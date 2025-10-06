48f8158918a364ff789ba17f97628501f4edb544
natashalawford <natasha.lawford@gmail.com>
2025-10-06 18:30:33 -0400

    DELETE undocumented endpoint

---
+++ JavaTestingAPI/src/test/java/com/ecse429/restapi/JsonTests/undocumentedEndpoints/deleteProjectsTasksTest.java
@@ -0,0 +1,26 @@
+package com.ecse429.restapi.JsonTests.undocumentedEndpoints;
+
+import com.ecse429.restapi.BaseApiTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.CoreMatchers.is;
+
+
+// Undocumented endpoint test: DELETE /projects/tasks
+// Assumes the server is started externally with default configuration.
+
+public class deleteProjectsTasksTest extends BaseApiTest {
+
+    @Test
+    void deleteProjectsTasksShouldReturn200or204() {
+        given()
+            .accept(ContentType.JSON)
+        .when()
+            .delete("/projects/tasks")
+        .then()
+            .statusCode(anyOf(is(200), is(204)));
+    }
+}
